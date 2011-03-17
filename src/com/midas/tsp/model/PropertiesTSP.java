2e104e95f43d300df71b5265ccc721128113f414
carlosivanduarte@gmail.com <carlosivanduarte@gmail.com>
2011-03-17 21:10:24 +0000

    Se ajustan Utilities y clases modelo, pendiente test cases

---
+++ src/com/midas/tsp/model/PropertiesTSP.java
@@ -0,0 +1,22 @@
+package com.midas.tsp.model;
+
+public class PropertiesTSP {
+	private String id;
+	private String description;
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+}
--- src/com/midas/tsp/util/Utility.java
+++ src/com/midas/tsp/util/Utility.java
@@ -1,17 +1,22 @@
 package com.midas.tsp.util;
 
+import static com.midas.tsp.Constants.SEPARATOR_ID;
+import static com.midas.tsp.Constants.SEPARATOR_PROPERTIES;
+import static com.midas.tsp.Constants.TASK_PROPERTIES;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.List;
 import java.util.Map.Entry;
 import java.util.Properties;
 
-import com.midas.tsp.Constants;
 import com.midas.tsp.annotations.LogT;
 import com.midas.tsp.annotations.LogTs;
 import com.midas.tsp.exceptions.TSPException;
+import com.midas.tsp.model.PropertiesTSP;
+import com.midas.tsp.model.Task;
 
 /**
  * @author German Dario Camacho S.
@@ -38,7 +43,7 @@ public class Utility {
 	 * @param map
 	 * @return
 	 * @throws TSPException
-	 */
+	 *//*
 	@LogTs({@LogT(cycle=1, date="14/03/2011", id="999", time=20, who="CIDC")})
 	public static Properties convertMapToProperties(Map<String, LinkedList<String>> map)
 			throws TSPException {
@@ -62,12 +67,12 @@ public class Utility {
 		return prop;
 	}
 	
-	/**
+	*//**
 	 * Permite convertir un Properties en un mapa
 	 * @param prop
 	 * @return
 	 * @throws TSPException
-	 */
+	 *//*
 	@LogTs({@LogT(cycle=1, date="14/03/2011", id="999", time=15, who="CIDC")})
 	public static Map<String, LinkedList<String>> convertPropertiesToMap(Properties prop) throws TSPException{
 		Map<String, LinkedList<String>> map = null;
@@ -84,6 +89,66 @@ public class Utility {
 			throw new TSPException(ex);
 		}
 		return map;
+	}*/
+	
+	@LogTs({@LogT(cycle=2, date="17/04/2011", id="???", time=20, who="CIDC")})
+	public static Properties convertToProperties(List<? extends PropertiesTSP> propertiesTSP) throws TSPException {
+		Properties properties = null;
+		try {
+			properties = new Properties();
+			for (PropertiesTSP propTSP : propertiesTSP) {
+				if (propTSP instanceof Task) {
+					Task task = (Task)propTSP;
+					properties.setProperty(task.getCycle() + SEPARATOR_ID + task.getId(), 
+									 task.getDescription() + SEPARATOR_PROPERTIES + 
+									 task.getDuration() + SEPARATOR_PROPERTIES + 
+									 task.getSize());					
+				}
+				else {
+					properties.setProperty(propTSP.getId(), propTSP.getDescription());
+				}
+			}
+		}
+		catch (Exception ex) {
+			throw new TSPException(ex);
+		}
+		return properties;
+	}
+	
+	@LogTs({@LogT(cycle=2, date="17/04/2011", id="???", time=30, who="CIDC")})
+	public static List<? extends PropertiesTSP> convertoToPropertiesTSP(File file) throws TSPException {
+		List<PropertiesTSP> propertiesTSP = null;
+		Properties properties = null;
+		try {
+			properties = new Properties();
+			FileInputStream stream = new FileInputStream(file);
+			properties.load(stream);
+			propertiesTSP = new LinkedList<PropertiesTSP>();
+			for (Entry<Object, Object> entry : properties.entrySet()) {
+				String key = (String)entry.getKey();
+				String[] attributes = ((String)entry.getValue()).split(SEPARATOR_PROPERTIES);
+				if (file.getName().equals(TASK_PROPERTIES)) {
+					String[] id = key.split(SEPARATOR_ID);
+					Task task = new Task();
+					task.setCycle(Integer.parseInt(id[0]));
+					task.setId(id[1]);
+					task.setDescription(attributes[0]);
+					task.setSize(Integer.parseInt(attributes[1]));
+					task.setDuration(Integer.parseInt(attributes[2]));
+					propertiesTSP.add(task);
+				}			
+				else {
+					PropertiesTSP propTSP = new PropertiesTSP();
+					propTSP.setId(key);
+					propTSP.setDescription(attributes[0]);
+					propertiesTSP.add(propTSP);
+				}
+			}		
+		}
+		catch (Exception ex) {
+			throw new TSPException(ex);
+		}
+		return propertiesTSP;
 	}
 
 }
