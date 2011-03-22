4d0da5e580cd86e442118fce57c99803bcf592f9
carlosivanduarte@gmail.com <carlosivanduarte@gmail.com>
2011-03-22 21:04:29 +0000

    Se crea JUnit Para clase BasicDAO

---
+++ test/src/model/dao/test/BasicDAOTest.java
@@ -0,0 +1,65 @@
+package src.model.dao.test;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.midas.tsp.annotations.Loc;
+import com.midas.tsp.annotations.LocControl;
+import com.midas.tsp.model.dao.BasicDAO;
+
+/**
+ * @author CIDC
+ * @date 22/03/2011
+ *
+ */
+public class BasicDAOTest {
+
+	/**
+	 * Constante con el path del properties a Probar
+	 */
+	private static final String PATH_PROPERTIES = "/Volumes/Archivos/ECOS_NotSync/workspace/midas-tsp/properties/defects.properties";
+	/**
+	 * Almancena Instancia BasicDAO
+	 */
+	private BasicDAO basicDAO;
+
+	/**
+	 * Inicializa variables
+	 * @throws Exception
+	 */
+	@Before
+	@Loc(size=1, type=LocControl.LocType.NUEVA, who="CIDC")
+	public void setUp() throws Exception {
+		basicDAO = new BasicDAO(PATH_PROPERTIES);
+	}
+
+	/**
+	 * Prueba método BasicDAO.save
+	 * @throws Exception
+	 */
+	@Test
+	@Loc(size=6, type=LocControl.LocType.NUEVA, who="CIDC")
+	public void testSave() throws Exception {
+		Properties props = new Properties();
+		FileInputStream fis = null;
+		fis = new FileInputStream(PATH_PROPERTIES);
+		props.load(fis);
+		fis.close();
+		basicDAO.save(props);
+	}
+
+	/**
+	 * Prueba método BasicDAO.load
+	 * @throws Exception
+	 */
+	@Test
+	@Loc(size=2, type=LocControl.LocType.NUEVA, who="CIDC")
+	public void testLoad() throws Exception {
+		Properties prop = basicDAO.load();
+		Assert.assertNotNull(prop);
+	}
+}
